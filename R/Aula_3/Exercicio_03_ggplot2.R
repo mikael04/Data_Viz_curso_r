dfe101813dcaff21aa39dd28a82eeb226e4c6735
mikael04 <mikael.coletto.eng@gmail.com>
2021-08-16 18:30:23 -0300

    Adicionando arquivos novos;

---
+++ R/Aula_3/Exercicio_03_ggplot2.R
@@ -0,0 +1,3 @@
+## 2. Qual o propósito do YAML do documento?
+## R. O YAML é o cabeçalho, ele possui alguns parâmetros pré-definidos que guiarão
+## o RStudio a identificar o documento e como proceder nele, usado para gerar o arquivo final
